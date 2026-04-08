a1926793851f010ea05ea7259ff842d4766b7af7
Siri <siriregonda@gmail.com>
2026-04-08 21:32:20 +0530

    added app in main

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ from flask import Flask, request, jsonify
 from environment import ContentEnv
 
 app = Flask(__name__)
+
 env = ContentEnv()
 
 @app.route("/")
 def home():
     return "API running"
 
-@app.route("/reset", methods=["GET"])
+@app.route("/reset", methods=["POST"])
 def reset():
-    return jsonify(env.reset())
+    state = env.reset()
+    return jsonify(state)
 
 @app.route("/step", methods=["POST"])
 def step():
-    data = request.get_json()
+    data = request.json
     action = data.get("action")
-    state, reward, done = env.step(action)
+
+    state, reward, done, info = env.step(action)
+
     return jsonify({
         "state": state,
         "reward": reward,
